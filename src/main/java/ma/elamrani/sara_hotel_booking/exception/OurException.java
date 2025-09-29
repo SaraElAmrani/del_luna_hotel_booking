8d57eb813a71c4ce4cc8e2ba986c2fa052c1e43a
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-09-29 07:41:47 +0100

    feat: add custom runtime exception OurException

---
+++ src/main/java/ma/elamrani/sara_hotel_booking/exception/OurException.java
@@ -0,0 +1,8 @@
+package ma.elamrani.sara_hotel_booking.exception;
+
+public class OurException extends RuntimeException {
+
+    public OurException(String message) {
+        super(message);
+    }
+}
