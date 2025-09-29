bc9f417d724b66d54ef2a513511b9a70ce661e27
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-09-29 07:20:34 +0100

    feat: add Response and LoginRequest DTOs for API handling

---
+++ src/main/java/ma/elamrani/sara_hotel_booking/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package ma.elamrani.sara_hotel_booking.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+}
