15d7bc582b260330ff32f206b2cf7a0ec131821e
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-09-29 06:19:12 +0100

    Initial commit

---
+++ src/main/java/ma/elamrani/sara_hotel_booking/SaraHotelBookingApplication.java
@@ -0,0 +1,13 @@
+package ma.elamrani.sara_hotel_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraHotelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaraHotelBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/elamrani/sara_hotel_booking/SaraHotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elamrani.sara_hotel_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaraHotelBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
